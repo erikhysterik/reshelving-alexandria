e7902aa8e3f79ce272f985718edf9faa363431bc
Erik Sautter <erikhysterik@users.noreply.github.com>
2025-09-06 23:35:00 -0500

    Fix React import error: add missing React import to author page component

--- app/legacy-library/author/[reference]/page.js
+++ app/legacy-library/author/[reference]/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import React from 'react'
 import { notFound } from 'next/navigation'
 import PageWrapper from '../../../../components/PageWrapper'
 import SearchWidget from '../../../../components/SearchWidget'
