1acba8eb706c3da3f59d891a177f34ebee24e188
Erik Sautter <erikhysterik@users.noreply.github.com>
2025-09-06 22:39:43 -0500

    Fix Link href undefined error: add null checks and default values to SearchResult component

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -11,26 +11,33 @@ import { slugify } from '@sindresorhus/slugify'
 
 const HitCount = connectStateResults(({ searchState, searchResults }) => {
   const hitCount = searchResults && searchResults.nbHits
+  const query = searchState?.query || ''
 
   return hitCount > 0 ? (
     <div className="HitCount">
-      <Link href={`/legacy-library/search/?q=${searchState.query}`}>
+      <Link href={`/legacy-library/search/?q=${encodeURIComponent(query)}`}>
         {hitCount} result{hitCount !== 1 ? `s` : ``}
       </Link>
     </div>
   ) : null
 })
 
-const PageHit = ({ hit }) => (
-  <div>
-    <Link href={`/legacy-library/book/${slugify(hit.reference)}`}>
-      <h4>
-        <Highlight attribute="title" hit={hit} tagName="mark" />
-      </h4>
-    </Link>
-    <Snippet attribute="excerpt" hit={hit} tagName="mark" />
-  </div>
-)
+const PageHit = ({ hit }) => {
+  if (!hit || !hit.reference) {
+    return null
+  }
+
+  return (
+    <div>
+      <Link href={`/legacy-library/book/${slugify(hit.reference)}`}>
+        <h4>
+          <Highlight attribute="title" hit={hit} tagName="mark" />
+        </h4>
+      </Link>
+      <Snippet attribute="excerpt" hit={hit} tagName="mark" />
+    </div>
+  )
+}
 
 const HitsInIndex = ({ index }) => (
   <Index indexName={index.name}>
