cd4fe6e23beda0acd314c9acc35493e6ca7bc5ec
Erik Sautter <erikhysterik@users.noreply.github.com>
2025-09-06 23:22:19 -0500

    Fix BookDetails component: add 'use client' directive to support SendToNotionButton with useState

--- components/BookDetails.js
+++ components/BookDetails.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react'
 import { Badge, Container, Row, Col, Breadcrumb, BreadcrumbItem, Card, Accordion, Button } from "react-bootstrap"
 import Link from 'next/link'
